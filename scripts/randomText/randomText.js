8455573e9c63922d7420f2d695e9a8d8ae6a4dc6
Turnstiles <skylinesturnstiles@outlook.com>
2025-04-18 16:55:26 -0400

    attempt to fix randomText script

--- scripts/randomText/randomText.js
+++ scripts/randomText/randomText.js
@@ -1,8 +1,8 @@
-const images = ["/img/COMLAG.jpg"];
+const images = ["img/COMLAG.jpg"];
 const randomImage = images[Math.floor(Math.random() * images.length)];
 document.querySelector(".background").style.backgroundImage = `url(${randomImage})`;
 
-fetch('/scripts/randomText/randomText.txt')
+fetch('scripts/randomText/randomText.txt')
   .then(response => response.text())
   .then(text => {
     const lines = text.split('\n').filter(line => line.trim() && !line.trim().startsWith('//'));
