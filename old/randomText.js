8cae4902ad626d62519380c9fdc9e2d6c1dfc54b
Turnstiles <skylinesturnstiles@outlook.com>
2025-04-18 17:09:58 -0400

    im just gonna redo the site smh my head

--- old/randomText.js
+++ old/randomText.js
@@ -1,4 +1,4 @@
-fetch('/2plus2isfive/scripts/randomText/randomText.txt')
+fetch('https://github.com/2plus2isfive/2plus2isfive/raw/db5044d8e4f7872e6430bc97af96382ad6554114/scripts/randomText/randomText.txt')
   .then(response => response.text())
   .then(text => {
     const lines = text.split('\n').filter(line => line.trim() && !line.trim().startsWith('//'));
