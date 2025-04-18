83b2c203344d3f40bcf4f94c370577baf0056a03
Turnstiles <skylinesturnstiles@outlook.com>
2025-04-18 18:21:03 -0400

    wiping most

--- old/randomText.js
+++
@@ -1,7 +0,0 @@
-fetch('https://github.com/2plus2isfive/2plus2isfive/raw/db5044d8e4f7872e6430bc97af96382ad6554114/scripts/randomText/randomText.txt')
-  .then(response => response.text())
-  .then(text => {
-    const lines = text.split('\n').filter(line => line.trim() && !line.trim().startsWith('//'));
-    const randomLine = lines[Math.floor(Math.random() * lines.length)];
-    document.getElementById('randomText').innerText = randomLine;
-  });
